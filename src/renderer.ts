e638d05f657aeed9f4399ef62f3c092a1a3239ee
godpiao <godpiao@qq.com>
2024-04-21 19:52:35 +0800

    preload 版本号显示

--- src/renderer.ts
+++ src/renderer.ts
@@ -28,4 +28,17 @@
 
 import './index.css';
 
+declare global {
+    interface Window {
+      versions?: {
+        electron: ()=>string;
+        chrome: ()=>string;
+        node: ()=>string;
+      };
+    }
+  }
+  
+
+const information = document.getElementById('info')
+information.innerText = `This app is using Chrome (v${window.versions.chrome()}), Node.js (v${window.versions.node()}), and Electron (v${window.versions.electron()})`
 console.log('👋 This message is being logged by "renderer.ts", included via Vite');
